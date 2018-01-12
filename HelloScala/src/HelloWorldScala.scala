a3cc9ae058c6a09f999173a00436b542b2d2550c
krishstrings <krishstrings@gmail.com>
2018-01-12 11:07:15 -0800

    added basic programs

---
+++ HelloScala/src/HelloWorldScala.scala
@@ -0,0 +1,10 @@
+
+
+object HelloWorldScala {
+  def main(args: Array[String]) {
+    var str = "Hello World";
+    str = "New String";
+     println(str);
+
+  }
+}
---
+++ JAVAExamples/src/com/examples/HelloWorld.java
@@ -0,0 +1,9 @@
+package com.examples;
+
+public class HelloWorld {
+	public static void main(String args[]) {
+		System.out.println("Hi There");
+		System.out.println(System.getProperty("java.version"));
+	}
+
+}
