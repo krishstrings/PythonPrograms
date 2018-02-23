1d07215443dd55e987787c76b92552e0ae4c6816
krishstrings <krishstrings@gmail.com>
2018-02-23 11:34:49 -0800

    made a few changes

---
+++ tst.py
@@ -0,0 +1,8 @@
+import pandas as pd 
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import roc_curve
+import matplotlib
+import matplotlib.pyplot as plt
+from IPython.display import display, HTML
