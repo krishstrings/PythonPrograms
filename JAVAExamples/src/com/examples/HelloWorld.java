668c93e0b5cd8a02ee746131356118dbc9f268e7
krishstrings <krishstrings@gmail.com>
2018-02-23 11:36:18 -0800

    made a few changes

--- JAVAExamples/src/com/examples/HelloWorld.java
+++
@@ -1,9 +0,0 @@
-package com.examples;
-
-public class HelloWorld {
-	public static void main(String args[]) {
-		System.out.println("Hi There");
-		System.out.println(System.getProperty("java.version"));
-	}
-
-}
